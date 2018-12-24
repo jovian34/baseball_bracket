a8c8aefd56b8931538af14a203dfde3182ca6b2a
Carl James <carl@jovian34.com>
2018-12-24 12:56:44 -0500

    data parsing working for nolan

--- data_collect/get_current_rpi_wn.py
+++ data_collect/get_current_rpi_wn.py
@@ -39,11 +39,12 @@ def break_into_sub_lists(td_text_limited):
 def create_nolan_dict(td_team_lists):
     nolan_dict = {}
     for team_list in td_team_lists:
-        rank = int(team_list[0])
+        rpi_rank = int(team_list[0])
+
         team_items = team_list[2].split('\n')
-        name = team_items[0].strip().upper()
-        conf_info = team_items[2].split(' (')
-        conference = conf_info[0].strip
+        team_name = team_items[0].strip().upper()
+        conf_info = team_items[1].split(' (')
+        conference = conf_info[0].strip()
         conf_record = conf_info[1].strip(')')
         conf_record = conf_record.split('-')
         conf_wins = int(conf_record[0])
@@ -52,18 +53,48 @@ def create_nolan_dict(td_team_lists):
             conf_ties = int(conf_record[2])
         else:
             conf_ties = 0
-        total_record = team_items[3].split('-')
+
+        total_record = team_list[3].split('-')
         total_wins = int(total_record[0])
         total_losses = int(total_record[1])
         if len(total_record) == 3:
-            total_ties = total_record[2]
+            total_ties = int(total_record[2])
         else:
             total_ties = 0
-        rpi = float(team_items[5])
-        sos_rank = int(team_items[6])
-        sos = float(team_items[7])
-        nc_record = team_items[8].split('-')
 
+        rpi = float(team_list[5])
+        sos_rank = int(team_list[6])
+        sos = float(team_list[7])
+
+        nc_record = team_list[8].split('-')
+        nc_wins = int(nc_record[0])
+        nc_losses = int(nc_record[1])
+        if len(nc_record) == 3:
+            nc_ties = int(nc_record[2])
+        else:
+            nc_ties = 0
+
+        nc_rpi_rank = int(team_list[9])
+        nc_sos_rank = int(team_list[10])
+
+        nolan_dict[team_name] = {
+            'rpi_rank': rpi_rank,
+            'conference': conference,
+            'conf_wins': conf_wins,
+            'conf_losses': conf_losses,
+            'conf_ties': conf_ties,
+            'total_wins': total_wins,
+            'total_losses': total_losses,
+            'total_ties': total_ties,
+            'rpi': rpi,
+            'sos_rank': sos_rank,
+            'sos': sos,
+            'nc_wins': nc_wins,
+            'nc_losses': nc_losses,
+            'nc_ties': nc_ties,
+            'nc_rpi_rank': nc_rpi_rank,
+            'nc_sos_rank': nc_sos_rank,
+        }
     return nolan_dict
 
 
@@ -81,4 +112,6 @@ if __name__ == "__main__":
     nolan_dict = main()
     for name, items in nolan_dict.items():
         print(f"{name}")
-        print(items)
+        for item, value in items.items():
+            print(f"     {item}: {value}")
+        print("============================")
