66711e6150f0f504943dd5e97fe918e0cd37934c
Carl James <carl@jovian34.com>
2018-12-27 23:52:03 -0500

    placed name fix logic in data collection package

--- baseball_bracket/main.py
+++ baseball_bracket/main.py
@@ -1,38 +1,7 @@
 from baseball_bracket.data_collect.get_current_rpi_wn import NolanRpiExpand
 from baseball_bracket.data_collect.get_current_isr_bw import BoydsIsr
-from baseball_bracket.data_collect.name_map import boyds_to_nolan
-
-
-def change_mismatched_keys(isr):
-    dict_fixed = {}
-    for key, value in isr.items():
-        if key not in boyds_to_nolan:
-            dict_fixed[key] = value
-        else:
-            dict_fixed[boyds_to_nolan[key]] = value
-    print(f'Size of ISR dict fixed inside of function is {len(dict_fixed)}')
-    return dict_fixed
-
-
-def find_inconsistent_names(rpi, isr):
-    bad_isr_names = []
-    good_names = []
-    bad_rpi_names = []
-
-    for key in isr:
-        if key in rpi:
-            good_names.append(key)
-        else:
-            bad_isr_names.append(key)
-
-    for key in rpi:
-        if key not in good_names:
-            bad_rpi_names.append(key)
-
-    if len(bad_isr_names) == len(bad_rpi_names) == 0:
-        return False
-    else:
-        return True
+from baseball_bracket.data_collect.name_difference import change_mismatched_keys
+from baseball_bracket.data_collect.name_difference import find_inconsistent_names
 
 
 def main():
