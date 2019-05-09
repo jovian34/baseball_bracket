c66125e7da338bcedcd837c397f17fc399c3cc7e
Carl James <carl@jovian34.com>
2019-05-09 12:35:44 -0400

    manually set conf tiers so to lower conf adj for lesser confs

--- baseball_bracket/calculations/conference_adjustment.py
+++ baseball_bracket/calculations/conference_adjustment.py
@@ -1,4 +1,5 @@
-#power6 = ['SEC', 'Big 12', 'ACC', 'American Athletic', 'Pac-12', 'Big Ten', ]
+power4 = ['SEC', 'Big 12', 'ACC', 'Pac-12', ]
+next4 = ['American Athletic', 'Big Ten', 'Missouri Valley', 'West Coast', ]
 
 
 class ConferenceAdjustment:
@@ -9,13 +10,18 @@ class ConferenceAdjustment:
 
     def adjust_rpi_for_conf_play(self):
         for key, value in self.team_dict.items():
-            if value['conf_win_pct'] <= 0.5:
+            if value['conf_win_pct'] < 0.5:
                 # second lines are midseason adjustments top lines used in May
                 self.team_dict[key]['adjusted_rpi'] += -0.015
                 # self.team_dict[key]['adjusted_rpi'] += -0.0075
             if value['conf_champs']:
-                self.team_dict[key]['adjusted_rpi'] += 0.018
-                # self.team_dict[key]['adjusted_rpi'] += 0.009
+                if value['conference'] in power4:
+                    self.team_dict[key]['adjusted_rpi'] += 0.018
+                    # self.team_dict[key]['adjusted_rpi'] += 0.009
+                elif value['conference'] in next4:
+                    self.team_dict[key]['adjusted_rpi'] += 0.013
+                else:
+                    self.team_dict[key]['adjusted_rpi'] += 0.009
 
     def return_team_dict(self):
         return self.team_dict
